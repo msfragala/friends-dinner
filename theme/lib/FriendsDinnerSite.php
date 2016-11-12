d5c45d5679526b5ecbb570276fd6eecfc137f950
Mitchell Gates <ms.fragala@gmail.com>
2016-11-12 17:44:38 -0500

    Add Recipe post type

--- theme/lib/FriendsDinnerSite.php
+++ theme/lib/FriendsDinnerSite.php
@@ -45,7 +45,35 @@ class FriendsDinnerSite {
    * Define custom post types
    */
   function register_post_types() {
-
+    register_post_type('recipe', array(
+      'labels' => array(
+        'name' => 'Recipes',
+        'singular_name' => 'Recipe',
+        'menu_name' => 'Recipes',
+        'name_admin_bar' => 'Recipe',
+        'add_new' => 'Add New',
+        'add_new_item' => 'Add New Recipe',
+        'new_item' => 'New Recipe',
+        'edit_item' => 'Edit Recipe',
+        'view_item' => 'View Recipe',
+        'all_items' => 'All Recipes',
+        'search_items' => 'Search Recipes',
+        'not_found' => 'No recipes found.',
+        'not_found_in_trash' => 'No recipes found in Trash.'
+      ),
+      'public' => true,
+      'publicly_queryable' => true,
+      'show_ui' => true,
+      'show_in_menu' => true,
+      'query_var' => false,
+      'rewrite' => array( 'slug' => 'recipes' ),
+      'has_archive' => false,
+      'exclude_from_search' => true,
+      'hierarchical' => false,
+      'menu_position' => 5,
+      'menu_icon' => 'dashicons-carrot',
+      'supports' => array('title', 'thumbnail')
+    ));
   }
 
   /**
